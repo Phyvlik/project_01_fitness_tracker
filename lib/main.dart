fe19c84d3effcd8ee101da19c831d356b3bd5e87
Vivek Patel <vivekschpatel@gmail.com>
2026-03-23 01:17:01 -0400

    Fix web SQLite initialization for Chrome runtime

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:provider/provider.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
 import 'providers/app_provider.dart';
 import 'widgets/main_navigation.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // sqflite needs an explicit database factory on web.
+  if (kIsWeb) {
+    databaseFactory = databaseFactoryFfiWeb;
+  }
+
   final appProvider = AppProvider();
   await appProvider.init();
   runApp(MyApp(appProvider: appProvider));
